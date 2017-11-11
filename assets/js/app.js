ea8cfe230f4c9c6fcaf5f1d24e8e8b4618eaf354
IreeRodriguez <ire208@gmail.com>
2017-11-11 18:52:24 -0300

    fondo y header

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,10 @@
+function show() {
+    var boton = document.getElementById("mostrar")
+    boton.style.display="none";
+    document.getElementsByClassName("create")[0].style.display="inline-block";
+}
 window.onload = function() {
+
     var guardar = document.getElementById("guardar");
     var newList = document.getElementById("newList");
     var section = document.getElementsByTagName("section")[0];
@@ -7,19 +13,25 @@ window.onload = function() {
         if (newList.value == "") {
             return false;
         } else {
-
             var lista = document.createElement("div");
             var nombreLista = document.createElement("h3");
             var tarjeta = document.createElement("textarea");
+            var btn = document.createElement("button")
+            btn.textContent="Añadir";
+            btn.id = "addCard";
+            tarjeta.placeholder="Anadir tarjeta";
             nombreLista.textContent = newList.value;
             lista.appendChild(nombreLista);
             lista.appendChild(tarjeta);
+            lista.appendChild(btn);
             section.insertBefore(lista, section.children[0]);
             document.getElementById("newList").value = "";
+            document.getElementById("mostrar").style.display="inline-block";
+            document.getElementsByClassName("create")[0].style.display="none";
         }
-
-
     }
+
+
 
 
 }
