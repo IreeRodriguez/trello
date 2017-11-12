e0d5b98091dce61d2dc9dca42193727222193693
IreeRodriguez <ire208@gmail.com>
2017-11-11 21:22:14 -0300

    retoques de diseno

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,29 @@
 function show() {
+    //oculta boton y muetra el formulario para crear nuevas tarjetas//
     var boton = document.getElementById("mostrar")
     boton.style.display="none";
     document.getElementsByClassName("create")[0].style.display="inline-block";
 }
+
 window.onload = function() {
+    //funcion para crear nuevas tarjetas//
 
     var guardar = document.getElementById("guardar");
     var newList = document.getElementById("newList");
     var section = document.getElementsByTagName("section")[0];
+    var bot = document.getElementById("mostrar");
 
     guardar.onclick = function() {
         if (newList.value == "") {
+            //el boton solo funcionara si hay algo escrito en el input//
             return false;
         } else {
+            //se crean divs y los demas elemenos que componen el bloque de nuevas tarjetas//
             var lista = document.createElement("div");
             var nombreLista = document.createElement("h3");
             var tarjeta = document.createElement("textarea");
             var btn = document.createElement("button")
+            //se le agregan atributos y cotenidos//
             btn.textContent="Añadir";
             btn.classList.add("btn");
             btn.id = "addCard";
@@ -26,14 +33,18 @@ window.onload = function() {
             lista.appendChild(tarjeta);
             lista.appendChild(btn);
             lista.classList.add("newCreate");
-            section.insertBefore(lista, section.children[0]);
+            //se inserta adelante del boton para crear uno nuevo//
+            section.insertBefore(lista, bot);
+            //se borra el input para dejar espacio para crear uno nuevo//
             document.getElementById("newList").value = "";
+            //se oculta el bloque y se vuelve a mostrar el boton de añadir//
             document.getElementById("mostrar").style.display="inline-block";
             document.getElementsByClassName("create")[0].style.display="none";
 
             btn.addEventListener("click",function(){
 
                 if (tarjeta.value == "") {
+                    //el boton solo funcionara si hay algo escrito en el textarea//
                     return false;
                 } else {
 
@@ -47,9 +58,6 @@ window.onload = function() {
                 }
 
             })
-
-
-
         }
     }
 
